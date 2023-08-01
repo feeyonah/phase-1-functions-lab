0bcac1e4b13fb21ac21b963276603aca214ef7f4
fiona <omburafiona@gmail.com>
2023-08-01 21:56:51 +0300

    functions lab completed

---
+++ blunt.js
@@ -0,0 +1,11 @@
+funtion saturdayFun( activity= "rollerskate" ){
+console.log(`this saturday I want to go ${activity}!`);
+
+}
+function mondayWork( duty= "go to the office"){
+console.log(`this Monday I will ${duty}!`);
+
+
+
+
+}
